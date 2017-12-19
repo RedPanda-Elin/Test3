61c483f7cb438f1db488bf6be1cb4c2203b1536f
ElinDi <vivalatanya@gmail.com>
2017-12-19 13:18:25 +0500

    тыц тыц

---
+++ app/src/main/java/com/example/elin/test3/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.elin.test3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+//@Deprecated
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+    }
+}
